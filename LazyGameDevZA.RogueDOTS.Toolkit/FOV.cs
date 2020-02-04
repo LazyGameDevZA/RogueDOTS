f7e6d1a4cd64d62041d58ae3f08c8bb9e26db426
Matt Van Der Westhuizen <mattpwest@gmail.com>
2020-02-04 07:18:49 +0100

    fix(fov): Remove Bresenham line artifacts in FOV calculation
    Relatively simple postprocessing based on the algorithm described here:
    https://sites.google.com/site/jicenospam/visibilitydetermination
    
    fixes #1

--- LazyGameDevZA.RogueDOTS.Toolkit/FOV.cs
+++ LazyGameDevZA.RogueDOTS.Toolkit/FOV.cs
@@ -8,9 +8,10 @@ namespace LazyGameDevZA.RogueDOTS.Toolkit
     public static class FOV
     {
         // TODO Map might be switched out for an interface if possible
-        public static NativeHashSet<int2> FieldOfViewSet<T>(int2 start, int range, in T fovCheck) where T: IAlgorithm2D, IBaseMap
+        public static NativeHashSet<int2> FieldOfViewSet<T>(int2 start, int range, in T fovCheck)
+            where T : IAlgorithm2D, IBaseMap
         {
-            var visiblePoints = new NativeHashSet<int2>( range * 2 * range * 2, Allocator.Temp);
+            var visiblePoints = new NativeHashSet<int2>(range * 2 * range * 2, Allocator.Temp);
 
             var iterator = new BresenhamCircleNoDiag(start, range).GetEnumerator();
             
@@ -18,28 +19,74 @@ namespace LazyGameDevZA.RogueDOTS.Toolkit
             {
                 ScanFovLine(start, iterator.Current, fovCheck, ref visiblePoints);
             }
-            
+
             iterator.Dispose();
+
+            PostProcessToRemoveBresenhamArtifacts(start, range, fovCheck, visiblePoints);
 
             return visiblePoints;
         }
-        
-        public static NativeArray<int2> FieldOfView<T>(int2 start, int range, in T fovCheck) where T: IAlgorithm2D, IBaseMap
+
+        private static void PostProcessToRemoveBresenhamArtifacts<T>(int2 start, int range, in T fovCheck,
+            NativeHashSet<int2> visiblePoints) where T : IAlgorithm2D, IBaseMap
+        {
+            RemoveBresenhamArtifactsInDirection(start, new int2(-1, -1), range, fovCheck, visiblePoints);
+            RemoveBresenhamArtifactsInDirection(start, new int2(+1, -1), range, fovCheck, visiblePoints);
+            RemoveBresenhamArtifactsInDirection(start, new int2(-1, +1), range, fovCheck, visiblePoints);
+            RemoveBresenhamArtifactsInDirection(start, new int2(+1, +1), range, fovCheck, visiblePoints);
+        }
+
+        private static void RemoveBresenhamArtifactsInDirection<T>(
+            int2 start,
+            int2 direction,
+            int range,
+            in T fovCheck,
+            NativeHashSet<int2> visiblePoints) where T : IAlgorithm2D, IBaseMap
+        {
+            for (var i = 0; i <= range + 1; i++)
+            {
+                var x = start.x + i * direction.x;
+                for (var j = 0; j <= range + 1; j++)
+                {
+                    var y = start.y + j * direction.y;
+                    
+                    var current = new int2(x, y);
+                    var isWall = fovCheck.Inbounds(current) && fovCheck.IsOpaque(fovCheck.point2DToIndex(current));
+                    var horizontalInFront = new int2(x - direction.x, y);
+                    var verticalInFront = new int2(x, y - direction.y);
+                    var horizontalInFrontIsLitGround = fovCheck.Inbounds(horizontalInFront) && 
+                                          !fovCheck.IsOpaque(fovCheck.point2DToIndex(horizontalInFront)) &&
+                                          visiblePoints.Contains(horizontalInFront);
+                    var verticalInFrontIsLitGround = fovCheck.Inbounds(verticalInFront) &&
+                                           !fovCheck.IsOpaque(fovCheck.point2DToIndex(verticalInFront)) &&
+                                           visiblePoints.Contains(verticalInFront);
+
+                    if (isWall && (verticalInFrontIsLitGround || horizontalInFrontIsLitGround))
+                    {
+                        visiblePoints.TryAdd(current);
+                    }
+                }
+            }
+        }
+
+        public static NativeArray<int2> FieldOfView<T>(int2 start, int range, in T fovCheck)
+            where T : IAlgorithm2D, IBaseMap
         {
             return FieldOfViewSet(start, range, fovCheck).ToNativeArray();
         }
 
-        private static void ScanFovLine<T>(int2 start, int2 end, in T fovCheck, ref NativeHashSet<int2> visiblePoints) where T : IAlgorithm2D, IBaseMap
+        private static void ScanFovLine<T>(int2 start, int2 end, in T fovCheck, ref NativeHashSet<int2> visiblePoints)
+            where
+            T : IAlgorithm2D, IBaseMap
         {
             var line = VectorLine.New(start, end);
-
             var iterator = line.GetEnumerator();
+
             int2 target;
-            
-            while(iterator.MoveNext())
+            while (iterator.MoveNext())
             {
                 target = iterator.Current;
-                if(!fovCheck.Inbounds(target))
+                if (!fovCheck.Inbounds(target))
                 {
                     // We're outside the map
                     break;
@@ -47,13 +94,13 @@ namespace LazyGameDevZA.RogueDOTS.Toolkit
 
                 visiblePoints.TryAdd(target);
 
-                if(fovCheck.IsOpaque(fovCheck.point2DToIndex(target)))
+                if (fovCheck.IsOpaque(fovCheck.point2DToIndex(target)))
                 {
                     //FOV is blocked
                     break;
                 }
             }
-            
+
             iterator.Dispose();
         }
     }
